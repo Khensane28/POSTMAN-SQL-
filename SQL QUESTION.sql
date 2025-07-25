e963b34c50280c9e6c69303031417bc303bbb906
Khensane28 <150665459+Khensane28@users.noreply.github.com>
2025-07-25 16:22:11 +0200

    Update SQL QUESTION.sql

--- SQL QUESTION.sql
+++ SQL QUESTION.sql
@@ -1,31 +1,31 @@
+SELECT * FROM contacts
+WHERE first_name LIKE 'A%'
 
-SELECT * 
-FROM contacts 
-WHERE first_name LIKE 'A%';
+    SELECT contacts.id,contact_notes.contact_id
+FROM contacts
+LEFT JOIN contact_notes ON contacts.id = contact_notes.contact_id
+ORDER BY contacts.id
+WHERE created_at >=CURRENT_DATE -INTERVAL '7 days';
 
-SELECT contacts.id, contact_notes.contact_id 
-FROM contacts 
-LEFT JOIN contact_notes ON contacts.id = contact_notes.contact_id 
-WHERE created_at >= CURRENT_DATE - INTERVAL 7 DAY 
-ORDER BY contacts.id;
-
-SELECT contacts.first_name || ' ' || contacts.last_name AS full_name, 
-       contacts.email;
-
-SELECT contacts.id, contact_notes.contact_id 
-FROM contacts 
-LEFT JOIN contact_notes ON contacts.id = contact_notes.contact_id 
-WHERE created_at >= CURRENT_DATE - INTERVAL 7 DAY
-
-UNION
-
-SELECT contacts.id, contact_notes.contact_id 
-FROM contact_notes 
-RIGHT JOIN contacts ON contacts.id = contact_notes.contact_id 
-WHERE created_at >= CURRENT_DATE - INTERVAL 7 DAY 
-ORDER BY contacts.id;
-
-
-SELECT contacts.first_name || ' ' || contacts.last_name AS full_name,
-       contacts.emails, 
-       contact_notes.note_text AS most_recent_note;
+SELECT contacts.first_name +' ' + contacts.last_name as full_name,
+contacts.email,
+SELECT contacts.id,contact_notes.contact_id
+FROM contact
+FULL OUTER JOIN contact_notes ON contacts.id = contact_notes.contact_id
+ORDER BY contacts.id
+WHERE created_at >=CURRENT_DATE -INTERVAL '7 days';
+FINAL:
+SELECT
+contacts.first_name ||' '|| contacts.last_name AS full_name,
+contacts.emails,
+contact_notes.note_text AS most_recent_note
+FROM contacts
+JOIN(SELECT 
+contact_id,
+note_text,
+ROW_NUMBER() OVER (PARTITION BY contact_id ORDER BY created_at DESC) AS rn
+FROM contact_notes
+WHERE created at >= CURRENT_DATE - INTERVAL '7 days')AS n
+ON contact_notes.contact_id=contacts.id
+AND contact_notes.rn =1 WHERE contacts.first_name LIKE 'A%'
+ORDER BY full_name;
